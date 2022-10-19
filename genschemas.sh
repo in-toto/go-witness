94bd1bf620f8f9e09a596c12af3749a9e8d036a0
cole kennedy <colek42@gmail.com>
2022-10-19 13:54:30 -0500

    format schemas

--- genschemas.sh
+++ genschemas.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
 go run cmd/schemagen/main.go schemas
+
+#run jq to format the json schema
+for file in schemas/*.json; do
+    jq . $file > $file.tmp && mv $file.tmp $file
+done
+
 json2ts -i schemas/ -o types/
+
+
