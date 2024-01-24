6c1d8cfe8edf1f6ca91d3dc0221c9c6244787e16
chaosinthecrd <tom@tmlabs.co.uk>
2024-01-24 17:58:42 +0000

    fixing license headers
    Signed-off-by: chaosinthecrd <tom@tmlabs.co.uk>

--- signer/kms/aws/fakeclient.go
+++ signer/kms/aws/fakeclient.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package aws implement the interface with amazon aws kms service
 package aws
 
 import (
