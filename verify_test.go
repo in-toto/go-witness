2b6e32f464e9fb057be2a8b51a84319386fe5e68
John Kjell <john@testifysec.com>
2024-02-02 22:48:08 -0600

    Add licenses and fix golanglint-ci issues
    Signed-off-by: John Kjell <john@testifysec.com>

--- verify_test.go
+++ verify_test.go
@@ -1,3 +1,17 @@
+// Copyright 2024 The Witness Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package witness
 
 import (
@@ -23,7 +37,15 @@ func TestVerifyPolicySignature(t *testing.T) {
 	}
 
 	badRootCert, _, err := test.CreateRoot()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	rootCert, key, err := test.CreateRoot()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	leafCert, leafPriv, err := test.CreateLeaf(rootCert, key)
 	if err != nil {
 		t.Fatal(err)
