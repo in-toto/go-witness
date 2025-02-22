5b643bee583df540682f825cfb32663fcd627898
John Kjell <john@testifysec.com>
2025-02-22 14:49:41 -0600

    Preliminary Policy interface
    Signed-off-by: John Kjell <john@testifysec.com>

--- policy/policy.go
+++ policy/policy.go
@@ -18,21 +18,16 @@ import (
 	"bytes"
 	"context"
 	"crypto/x509"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
 	"time"
 
-	attestationv1 "github.com/in-toto/attestation/go/v1"
 	"github.com/in-toto/go-witness/attestation"
 	"github.com/in-toto/go-witness/cryptoutil"
-	"github.com/in-toto/go-witness/internal/policy_v2"
-	"github.com/in-toto/go-witness/log"
 	"github.com/in-toto/go-witness/signer"
 	"github.com/in-toto/go-witness/signer/kms"
 	"github.com/in-toto/go-witness/source"
-	"google.golang.org/protobuf/encoding/protojson"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -42,14 +37,17 @@ type PolicyPredicate string
 const WitnessPolicyPredicate PolicyPredicate = "https://witness.testifysec.com/policy/v0.1"
 const IntotoPolicyPredicate PolicyPredicate = "https://witness.in-toto.io/policy/v0.1"
 
+type Policy interface {
+	Verify(ctx context.Context, opts ...VerifyOption) (bool, map[string]StepResult, error)
+}
+
 // +kubebuilder:object:generate=true
-type Policy struct {
+type PolicyV1 struct {
 	Expires              metav1.Time          `json:"expires"`
 	Roots                map[string]Root      `json:"roots,omitempty"`
 	TimestampAuthorities map[string]Root      `json:"timestampauthorities,omitempty"`
 	PublicKeys           map[string]PublicKey `json:"publickeys,omitempty"`
 	Steps                map[string]Step      `json:"steps"`
-	Layout               *policy_v2.Layout
 }
 
 // +kubebuilder:object:generate=true
@@ -65,7 +63,7 @@ type PublicKey struct {
 }
 
 // PublicKeyVerifiers returns verifiers for each of the policy's embedded public keys grouped by the key's ID
-func (p Policy) PublicKeyVerifiers(ko map[string][]func(signer.SignerProvider) (signer.SignerProvider, error)) (map[string]cryptoutil.Verifier, error) {
+func (p PolicyV1) PublicKeyVerifiers(ko map[string][]func(signer.SignerProvider) (signer.SignerProvider, error)) (map[string]cryptoutil.Verifier, error) {
 	verifiers := make(map[string]cryptoutil.Verifier)
 	var err error
 
@@ -132,11 +130,11 @@ type TrustBundle struct {
 }
 
 // TrustBundles returns the policy's x509 roots and intermediates grouped by the root's ID
-func (p Policy) TrustBundles() (map[string]TrustBundle, error) {
+func (p PolicyV1) TrustBundles() (map[string]TrustBundle, error) {
 	return trustBundlesFromRoots(p.Roots)
 }
 
-func (p Policy) TimestampAuthorityTrustBundles() (map[string]TrustBundle, error) {
+func (p PolicyV1) TimestampAuthorityTrustBundles() (map[string]TrustBundle, error) {
 	return trustBundlesFromRoots(p.TimestampAuthorities)
 }
 
@@ -165,48 +163,48 @@ func trustBundlesFromRoots(roots map[string]Root) (map[string]TrustBundle, error
 	return bundles, nil
 }
 
-type VerifyOption func(*verifyOptions)
+type VerifyOption func(*VerifyOptions)
 
-type verifyOptions struct {
-	verifiedSource source.VerifiedSourcer
-	subjectDigests []string
-	searchDepth    int
+type VerifyOptions struct {
+	VerifiedSource source.VerifiedSourcer
+	SubjectDigests []string
+	SearchDepth    int
 }
 
 func WithVerifiedSource(verifiedSource source.VerifiedSourcer) VerifyOption {
-	return func(vo *verifyOptions) {
-		vo.verifiedSource = verifiedSource
+	return func(vo *VerifyOptions) {
+		vo.VerifiedSource = verifiedSource
 	}
 }
 
 func WithSubjectDigests(subjectDigests []string) VerifyOption {
-	return func(vo *verifyOptions) {
-		vo.subjectDigests = subjectDigests
+	return func(vo *VerifyOptions) {
+		vo.SubjectDigests = subjectDigests
 	}
 }
 
 func WithSearchDepth(depth int) VerifyOption {
-	return func(vo *verifyOptions) {
-		vo.searchDepth = depth
+	return func(vo *VerifyOptions) {
+		vo.SearchDepth = depth
 	}
 }
 
-func checkVerifyOpts(vo *verifyOptions) error {
-	if vo.verifiedSource == nil {
+func CheckVerifyOpts(vo *VerifyOptions) error {
+	if vo.VerifiedSource == nil {
 		return ErrInvalidOption{
 			Option: "verified source",
 			Reason: "a verified attestation source is required",
 		}
 	}
 
-	if len(vo.subjectDigests) == 0 {
+	if len(vo.SubjectDigests) == 0 {
 		return ErrInvalidOption{
 			Option: "subject digests",
 			Reason: "at least one subject digest is required",
 		}
 	}
 
-	if vo.searchDepth < 1 {
+	if vo.SearchDepth < 1 {
 		return ErrInvalidOption{
 			Option: "search depth",
 			Reason: "search depth must be at least 1",
@@ -216,16 +214,16 @@ func checkVerifyOpts(vo *verifyOptions) error {
 	return nil
 }
 
-func (p Policy) Verify(ctx context.Context, opts ...VerifyOption) (bool, map[string]StepResult, error) {
-	vo := &verifyOptions{
-		searchDepth: 3,
+func (p PolicyV1) Verify(ctx context.Context, opts ...VerifyOption) (bool, map[string]StepResult, error) {
+	vo := &VerifyOptions{
+		SearchDepth: 3,
 	}
 
 	for _, opt := range opts {
 		opt(vo)
 	}
 
-	if err := checkVerifyOpts(vo); err != nil {
+	if err := CheckVerifyOpts(vo); err != nil {
 		return false, nil, err
 	}
 
@@ -238,14 +236,10 @@ func (p Policy) Verify(ctx context.Context, opts ...VerifyOption) (bool, map[str
 		return false, nil, err
 	}
 
-	if p.Layout == nil {
-		return p.verifyV1(ctx, vo, trustBundles)
-	}
-
-	return p.verifyV2(ctx, vo, trustBundles)
+	return p.verifyV1(ctx, vo, trustBundles)
 }
 
-func (p Policy) verifyV1(ctx context.Context, vo *verifyOptions, trustBundles map[string]TrustBundle) (bool, map[string]StepResult, error) {
+func (p PolicyV1) verifyV1(ctx context.Context, vo *VerifyOptions, trustBundles map[string]TrustBundle) (bool, map[string]StepResult, error) {
 	attestationsByStep := make(map[string][]string)
 	for name, step := range p.Steps {
 		for _, attestation := range step.Attestations {
@@ -254,10 +248,10 @@ func (p Policy) verifyV1(ctx context.Context, vo *verifyOptions, trustBundles ma
 	}
 
 	resultsByStep := make(map[string]StepResult)
-	for depth := 0; depth < vo.searchDepth; depth++ {
+	for depth := 0; depth < vo.SearchDepth; depth++ {
 		for stepName, step := range p.Steps {
 			// Use search to get all the attestations that match the supplied step name and subjects
-			collections, err := vo.verifiedSource.Search(ctx, stepName, vo.subjectDigests, attestationsByStep[stepName])
+			collections, err := vo.VerifiedSource.Search(ctx, stepName, vo.SubjectDigests, attestationsByStep[stepName])
 			if err != nil {
 				return false, nil, err
 			}
@@ -285,7 +279,7 @@ func (p Policy) verifyV1(ctx context.Context, vo *verifyOptions, trustBundles ma
 			for _, coll := range stepResult.Passed {
 				for _, digestSet := range coll.Collection.BackRefs() {
 					for _, digest := range digestSet {
-						vo.subjectDigests = append(vo.subjectDigests, digest)
+						vo.SubjectDigests = append(vo.SubjectDigests, digest)
 					}
 				}
 			}
@@ -306,176 +300,6 @@ func (p Policy) verifyV1(ctx context.Context, vo *verifyOptions, trustBundles ma
 	}
 
 	return pass, resultsByStep, nil
-}
-
-func (p Policy) verifyV2(ctx context.Context, vo *verifyOptions, trustBundles map[string]TrustBundle) (bool, map[string]StepResult, error) {
-	// TODO: Add parameters to verifyOptions
-	// if len(parameters) > 0 {
-	// 	log.Info("Substituting parameters...")
-	// 	layout, err = substituteParameters(layout, parameters)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	log.Info("Done.")
-	// }
-
-	// Search for attestations by subjects only
-	// TODO: Add support for search depth
-	log.Info("loading attestations as claims...")
-	verifiedClaims := map[string]map[string]*attestationv1.Statement{}
-	for _, step := range p.Layout.Steps {
-		stepAttestations, err := vo.verifiedSource.Search(ctx, step.Name, vo.subjectDigests, nil)
-		if err != nil {
-			return false, nil, err
-		}
-
-		log.Infof("loading %d claims for %s...", len(stepAttestations), step.Name)
-		for _, attestation := range stepAttestations {
-			if verifiedClaims[step.Name] == nil {
-				verifiedClaims[step.Name] = make(map[string]*attestationv1.Statement)
-			}
-
-			statement := &attestationv1.Statement{}
-			// Use attestation.Envelope.Payload instead of attestation.Statement to start migrating towards upstream protobufs
-			if err := protojson.Unmarshal(attestation.Envelope.Payload, statement); err != nil {
-				return false, nil, fmt.Errorf("unable to load statement payload: %w", err)
-			}
-
-			if len(attestation.Verifiers) == 0 {
-				log.Infof("no valid functionaries found for attestation")
-			}
-
-			for _, ak := range attestation.Verifiers {
-				keyId, err := ak.KeyID()
-				if err != nil {
-					return false, nil, err
-				}
-				verifiedClaims[step.Name][keyId] = statement
-			}
-			log.Infof("loaded %d claims for %s\n", len(verifiedClaims[step.Name]), step.Name)
-			for _, err := range attestation.Errors {
-				log.Infof("error: %s", err)
-			}
-			for _, warning := range attestation.Warnings {
-				log.Infof("warning: %s", warning)
-			}
-		}
-	}
-
-	env, err := policy_v2.GetCELEnv()
-	if err != nil {
-		return false, nil, err
-	}
-
-	resultsByStep := make(map[string]StepResult)
-	for _, step := range p.Layout.Steps {
-		stepStatements, ok := verifiedClaims[step.Name]
-		if !ok {
-			return false, nil, fmt.Errorf("no claims found for step %s", step.Name)
-		}
-
-		if step.Threshold == 0 {
-			step.Threshold = 1
-		}
-
-		trustedStatements := policy_v2.GetPredicates(stepStatements, step.Functionaries)
-		if len(trustedStatements) < step.Threshold {
-			return false, nil, fmt.Errorf("threshold not met for step %s", step.Name)
-		}
-
-		// TODO: reduce statements if they're identical to avoid checking all of
-		// them
-		// See in-toto 1.0
-
-		acceptedPredicates := 0
-		failedChecks := []error{}
-		for functionary, statement := range trustedStatements {
-			log.Infof("Verifying claim for step '%s' of type '%s' by '%s'...", step.Name, step.ExpectedPredicateType, functionary)
-			failed := false
-
-			// Check the predicate type matches the expected value in the layout
-			if step.ExpectedPredicateType != statement.PredicateType {
-				failed = true
-				failedChecks = append(failedChecks, fmt.Errorf("for step %s, statement with unexpected predicate type %s found", step.Name, statement.PredicateType))
-			}
-
-			// Check materials and products
-			if err := policy_v2.ApplyArtifactRules(statement, step.ExpectedMaterials, step.ExpectedProducts, verifiedClaims); err != nil {
-				failed = true
-				failedChecks = append(failedChecks, fmt.Errorf("for step %s, claim by %s failed artifact rules: %w", step.Name, functionary, err))
-			}
-
-			input, err := policy_v2.GetActivation(statement)
-			if err != nil {
-				return false, nil, err
-			}
-
-			// Check attribute rules
-			if err := policy_v2.ApplyAttributeRules(env, input, step.ExpectedAttributes); err != nil {
-				failed = true
-				failedChecks = append(failedChecks, fmt.Errorf("for step %s, claim by %s failed attribute rules: %w", step.Name, functionary, err))
-			}
-
-			// Examine collector claims in attestation collection
-			if step.ExpectedPredicateType == attestation.CollectionType {
-				log.Infof("Verifying attestors for collection of step '%s'", step.Name)
-				collectionBytes, err := json.Marshal(statement.Predicate)
-				if err != nil {
-					return false, nil, err
-				}
-
-				collection := &attestation.Collection{}
-				if err := json.Unmarshal(collectionBytes, collection); err != nil {
-					return false, nil, err
-				}
-				log.Infof("Unmarshaled collection for step '%s'", step.Name)
-
-				// TODO: assumes only one of each attestor type
-				subAttestors := make(map[string]attestation.CollectionAttestation, len(collection.Attestations))
-				for _, subAttestor := range collection.Attestations {
-					subAttestors[subAttestor.Type] = subAttestor
-				}
-
-				env, err := policy_v2.GetCollectionCELEnv()
-				if err != nil {
-					return false, nil, err
-				}
-
-				for _, attestorConstraint := range step.ExpectedAttestors {
-					attestor, ok := subAttestors[attestorConstraint.AttestorType]
-					if !ok {
-						failed = true
-						failedChecks = append(failedChecks, fmt.Errorf("for step %s, attestor of type %s not found in collection", step.Name, attestorConstraint.AttestorType))
-						continue
-					}
-
-					input, err := policy_v2.GetCollectionActivation(&attestor)
-					if err != nil {
-						return false, nil, err
-					}
-
-					if err := policy_v2.ApplyAttributeRules(env, input, attestorConstraint.ExpectedAttributes); err != nil {
-						failed = true
-						failedChecks = append(failedChecks, fmt.Errorf("for step %s, claim by %s failed attribute rules for attestor %s: %w", step.Name, functionary, attestorConstraint.AttestorType, err))
-					}
-				}
-			}
-
-			if failed {
-				log.Infof("Claim for step %s of type %s by %s failed.", step.Name, step.ExpectedPredicateType, functionary)
-			} else {
-				acceptedPredicates += 1
-				log.Info("Done.")
-			}
-		}
-		if acceptedPredicates < step.Threshold {
-			return false, nil, errors.Join(failedChecks...)
-		}
-	}
-
-	log.Info("Verification successful!")
-
-	return true, resultsByStep, nil
 }
 
 // checkFunctionaries checks to make sure the signature on each statement corresponds to a trusted functionary for
@@ -515,7 +339,7 @@ func (step Step) checkFunctionaries(statements []source.CollectionVerificationRe
 
 // verifyArtifacts will check the artifacts (materials+products) of the step referred to by `ArtifactsFrom` against the
 // materials of the original step.  This ensures file integrity between each step.
-func (p Policy) verifyArtifacts(resultsByStep map[string]StepResult) (map[string]StepResult, error) {
+func (p PolicyV1) verifyArtifacts(resultsByStep map[string]StepResult) (map[string]StepResult, error) {
 	for _, step := range p.Steps {
 		accepted := false
 		if len(resultsByStep[step.Name].Passed) == 0 {
